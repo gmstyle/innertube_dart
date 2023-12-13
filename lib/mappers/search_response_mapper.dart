5064ee61d2a17bd6d54b4beb6944f9b9bc21dbda
Gabriele Martina <gabriele.martina@icsone.it>
2023-12-13 11:49:49 +0100

    Update search_response_mapper.dart

--- lib/mappers/search_response_mapper.dart
+++ lib/mappers/search_response_mapper.dart
@@ -6,7 +6,6 @@ import 'package:innertube_dart/models/responses/channel.dart';
 import 'package:innertube_dart/models/responses/playlist.dart';
 import 'package:innertube_dart/models/responses/search_response.dart';
 import 'package:innertube_dart/models/responses/video.dart';
-import 'package:innertube_dart/utils/utils.dart';
 
 class SearchResponseMapper
     extends BaseMapper<SearchResponse, Map<String, dynamic>> {
